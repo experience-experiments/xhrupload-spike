bb58de7ed49e19c39917341e65b4ffa4feb1c8a2
harunhasdal <harun.hasdal@rma-consulting.com>
2016-01-13 14:58:46 +0000

    Remove file type checking on drag event
    This feature doesn't work on Safari

--- examples/react-client/src/components/upload/XHRUpload.js
+++ examples/react-client/src/components/upload/XHRUpload.js
@@ -10,8 +10,9 @@ export default class XHRUpload extends React.Component {
       auto: React.PropTypes.bool,
       preview: React.PropTypes.bool,
       fieldName: React.PropTypes.string,
-      label: React.PropTypes.string,
-      accepts: React.PropTypes.string
+      buttonLabel: React.PropTypes.string,
+      dropzoneLabel: React.PropTypes.string,
+      debug: React.PropTypes.bool,
     };
   }
 
@@ -20,13 +21,16 @@ export default class XHRUpload extends React.Component {
       auto: false,
       preview: false,
       fieldName: 'datafile',
-      label: 'Upload'
+      buttonLabel: 'Upload',
+      dropzoneLabel: 'Click to select or Drag the file here',
+      debug: true
     };
   }
 
   constructor(props) {
     super(props);
     this.state = {file: null, progress: 0, log: []};
+    this.activeDrag = 0;
   }
 
   onClick() {
@@ -52,14 +56,8 @@ export default class XHRUpload extends React.Component {
 
   onDragEnter(e) {
     e.preventDefault();
-
-    const dataTransferItems = e.dataTransfer && e.dataTransfer.items ? e.dataTransfer.items : [];
-    const file = dataTransferItems[0];
-    const fileAccepted = this.fileAccepted(file);
-    this.setState({
-      isDragActive: fileAccepted,
-      isDragReject: !fileAccepted
-    });
+    this.activeDrag += 1;
+    this.setState({isActive: this.activeDrag > 0});
   }
 
   onDragOver(e) {
@@ -70,19 +68,16 @@ export default class XHRUpload extends React.Component {
 
   onDragLeave(e) {
     e.preventDefault();
-    this.setState({
-      isDragActive: false,
-      isDragReject: false
-    });
+    this.activeDrag -= 1;
+    if(this.activeDrag === 0) {
+      this.setState({isActive: false});
+    }
   }
 
   onDrop(e) {
     e.preventDefault();
-
-    this.setState({
-      isDragActive: false,
-      isDragReject: false
-    });
+    this.activeDrag = 0;
+    this.setState({isActive: false});
 
     const droppedFiles = e.dataTransfer ? e.dataTransfer.files : e.target.files;
     const file = droppedFiles[0];
@@ -96,15 +91,15 @@ export default class XHRUpload extends React.Component {
     });
   }
 
-  fileAccepted(file) {
-    if(this.props.accepts) {
-      return file.name.toLowerCase().endsWith(this.props.accepts);
+  getDropTargetClass() {
+    if(this.state.isActive) {
+      return 'XHRUpload__drop-target--active';
     }
-    return true;
+    return 'XHRUpload__drop-target';
   }
 
   upload(file) {
-    this.log(`Attempting to upload file ${file.name}`);
+    this.log(`Uploding ${file.name}`);
     const formData = new FormData();
     const xhr = new XMLHttpRequest();
 
@@ -126,28 +121,47 @@ export default class XHRUpload extends React.Component {
   }
 
   log(message) {
-    this.setState({log: [...this.state.log, message]});
+    if(this.props.debug) {
+      this.setState({log: [...this.state.log, message]});
+    }
   }
 
   renderFileDetails(file) {
     if(file) {
       const progress = this.state.progress;
-      const sizeInMB = (file.size / 1000000).toPrecision(2);
+      const sizeInMB = (file.size / (1024 * 1024)).toPrecision(2);
       return (
         <div className="XHRUpload__file-details">
-          <span className="icon-file">{`${file.name}, ${file.type}, ${sizeInMB}MB`}</span>
-          <progress className="XHRUpload__progress" min="0" max="100" value={progress}>{progress}%</progress>
+          <span className="icon-file">&nbsp;</span><span>{`${file.name}, ${file.type}, ${sizeInMB}MB`}</span>
+          {
+            this.state.progress > 0 ? <progress className="XHRUpload__progress" min="0" max="100" value={progress}>{progress}%</progress> : null
+          }
         </div>
       );
     }
-    return <p>Click to select or Drag the file here</p>;
+    return <p>{this.props.dropzoneLabel}</p>;
+  }
+
+  renderButton() {
+    const displayButton = !this.props.auto && this.state.progress < 100;
+    if(displayButton) {
+      return <button className="XHRUpload__upload-button" onClick={() => this.upload(this.state.file)}>{this.props.buttonLabel}</button>;
+    }
+    return null;
+  }
+
+  renderLog() {
+    if(this.props.debug) {
+      return <div className="logger">{this.state.log.map((item, i) => <pre key={i}>{item}</pre>)}</div>;
+    }
+    return null;
   }
 
   render() {
-    const displayButton = !this.props.auto && this.state.progress < 100;
+    const dropTargetClass = this.getDropTargetClass();
     return (
       <div className="XHRUpload">
-        <div className="XHRUpload__drop-target"
+        <div className={dropTargetClass}
           onClick={e => this.onClick(e)}
           onDragEnter={e => this.onDragEnter(e)}
           onDragOver={e => this.onDragOver(e)}
@@ -156,9 +170,9 @@ export default class XHRUpload extends React.Component {
         >
           {this.renderFileDetails(this.state.file)}
         </div>
-        <input className="XHRUpload__input" type="file" name="datasourcefile" ref="fileInput" onChange={() => this.onChange()}/>
-        {displayButton ? <button className="XHRUpload__upload-button" onClick={() => this.upload(this.state.file)}>Upload</button> : null}
-        <div className="logger">{this.state.log.map((item, i) => <pre key={i}>{item}</pre>)}</div>
+        {this.renderButton()}
+        <input className="XHRUpload__input" type="file" ref="fileInput" onChange={() => this.onChange()}/>
+        {this.renderLog()}
       </div>
     );
   }
