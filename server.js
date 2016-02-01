e908c07fb31bbd7f1e8bf1af045045b299435269
harunhasdal <harun.hasdal@rma-consulting.com>
2016-02-01 10:59:38 +0000

    Remove connection close header setting

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const app = express();
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.set('Connection', 'close'); // attempting to override default (keep-alive)
-  res.set('Proof', 'close');
   next();
  });
 
