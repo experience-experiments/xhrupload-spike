57a3757759d44faee956f799518d93633c45dd43
harunhasdal <harun.hasdal@rma-consulting.com>
2016-02-01 10:59:18 +0000

    Add encrypted upload

--- examples/react-client/src/index.js
+++ examples/react-client/src/index.js
@@ -5,12 +5,14 @@ import { Router, Route } from 'react-router';
 import { App } from './components/App';
 import Counter from './components/counter/Counter';
 import Uploader from './components/upload/Uploader';
+import EncryptedUpload from './components/upload/EncryptedUpload';
 
 import './index.scss';
 
 render(<Router>
   <Route path="/" component={App}>
     <Route path="upload" component={Uploader}/>
+    <Route path="encryption" component={EncryptedUpload}/>
     <Route path="counter" component={Counter}/>
   </Route>
 </Router>, document.getElementById('app'));
